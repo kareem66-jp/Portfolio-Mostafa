cc792d977ffc925b0fd6b8ed1d37702a77e7fdd9
kareem <kareem.mostfa994@gmail.com>
2026-09-11 16:07:14 +0200

    edite

--- pro.js
+++ pro.js
@@ -1055,3 +1055,14 @@ if (sp) {
 }
 
 // top bage
+
+// mouse effect
+
+// let glow = document.querySelector(".cursor-glow");
+
+// document.addEventListener("mousemove", (e) => {
+//   glow.style.left = e.clientX + "px";
+//   glow.style.top = e.clientY + "px";
+// });
+
+// mouse effect
